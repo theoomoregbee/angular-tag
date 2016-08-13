7d9ead36c212bf42bdae7db50cde22b961d800a3
Theophilus Omoregbee <theo4u@ymail.com>
2016-08-13 22:51:48 +0100

    other testing for error reporting

--- gulpfile.js
+++ gulpfile.js
@@ -80,7 +80,20 @@ gulp.task('concatenate',function () {
 
 gulp.task('build-js', function (callbacks) {
     runSequence('copy-js','concatenate','build-js-script',
-        callbacks
+        // this callback is executed at the end, if any of the previous tasks errored,
+        // the first param contains the error
+        function (err) {
+            //if any error happened in the previous tasks, exit with a code > 0
+            if (err) {
+                var exitCode = 2;
+                console.log('[ERROR] gulp build task failed', err);
+                console.log('[FAIL] gulp build task failed - exiting with code ' + exitCode);
+                return process.exit(exitCode);
+            }
+            else {
+                return callbacks();
+            }
+        }
     )
 });
 gulp.task('build-js-script', function() {
